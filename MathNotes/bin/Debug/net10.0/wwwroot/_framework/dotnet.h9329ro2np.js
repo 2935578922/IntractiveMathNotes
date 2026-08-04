1cdd1bd0508cee3f7fedd5160976795deeb630e5
Xiaoyu20104741 <xiaoyu20104741@noreply.codeberg.org>
2026-08-04 16:46:26 +0800

    更新跳转路径

--- MathNotes/bin/Debug/net10.0/wwwroot/_framework/dotnet.h9329ro2np.js
+++ MathNotes/bin/Debug/net10.0/wwwroot/_framework/dotnet.h9329ro2np.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "MathNotes",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-QnnoYTiC0UNK/awKRQT+v8IARKhLFCe5PmZh6je7ZRg=",
+    "hash": "sha256-/Cq+Hv5XyQScJ42/1LfSZXtqZ5V4cE5TlbnXb5r43Rw=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.rjbmzc4jpg.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "MathNotes.wasm",
-        "name": "MathNotes.zjci4f63lm.wasm",
-        "hash": "sha256-8a+KCdEiW9eKai+HrauHEQzSpbszAFjhmh4nWr3PVuw=",
+        "name": "MathNotes.7u7tdyscj1.wasm",
+        "hash": "sha256-EX4skmdXdhO0Kh/bN/og4ZCiX5eE64C1VCy8COHy3IM=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "MathNotes.pdb",
-        "name": "MathNotes.97ssaz4u0l.pdb",
-        "hash": "sha256-OwybRlQRqs7dWvlm/1Kh0d7dLmJnuGRcmIj/V5JMlCQ=",
+        "name": "MathNotes.dm8llos7ne.pdb",
+        "hash": "sha256-5TNR6zJxnjF/jMmi5jcbrHcEjnjPN0BWEBXxdNknnJ4=",
         "cache": "force-cache"
       }
     ],
--- MathNotes/obj/Debug/net10.0/MathNotes.AssemblyInfo.cs
+++ MathNotes/obj/Debug/net10.0/MathNotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8eddf1cb17ae609309c016b2efac615f2d5a321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95b032c85f402823c5f391cc88ca699cc6db1a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
