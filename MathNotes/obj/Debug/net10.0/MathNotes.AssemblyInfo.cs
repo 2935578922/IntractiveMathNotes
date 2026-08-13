a8928ded2ae8266cf872b158895109282d8d0f7f
Xiaoyu20104741 <xiaoyu20104741@noreply.codeberg.org>
2026-08-13 17:40:36 +0800

    调试

--- MathNotes/obj/Debug/net10.0/MathNotes.AssemblyInfo.cs
+++ MathNotes/obj/Debug/net10.0/MathNotes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbf47dc4db36f47c5baac849b81b008018fb2df7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+138561e8e4844f0dee6a821034867e540d5125a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathNotes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
